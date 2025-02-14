b7375a1932ee5e37de9da81203a9e1afae0b24ce
Dey Williams Gordillo <dey.gordillo@simpledatacorp.com>
2025-02-14 10:30:41 -0300

    Add middleware to catch errors and return it in json. Extend limit json to 50mb

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,18 @@ import config from './config/config';
 
 const app = express();
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack); // Log del error en la consola
+
+    res.status(err.status || 500).json({
+        code: err.status || 500,
+        message: err.message || "Internal Server Error",
+        error: process.env.NODE_ENV === "development" ? err : undefined, // Solo en desarrollo
+    });
+});
 
 app.use(indexRoutes);
 
